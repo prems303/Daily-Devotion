a63cfe7e113ea0879a5953287e20417bfe66ca89
Prem Sagar Thogaru <premsagarthogaru@Prems-iMac.local>
2016-08-18 15:41:49 +1200

    my first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+var hbs = require('express-handlebars')
+var path = require('path')
+var express = require('express')
+
+var app = express()
+
+app.use(express.static('public'))
+
+
+app.engine('hbs', hbs())
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+
+app.get("/", function (req, res) {
+  var server = {
+
+  }
+  res.render('index', server)
+})
+
+
+var PORT = process.env.PORT || 3000
+app.listen( PORT, function () {
+  console.log('The server is running on port: ', PORT)
+})
